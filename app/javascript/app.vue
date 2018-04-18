c35c838dc546367b9e883445a4da77e954da332d
Maloney L <maloneyl@users.noreply.github.com>
2018-04-18 18:07:28 +0100

    add an off-screen side nav
    ...only if the browser supports grid and the screen is wide enough
    
    also tweak header and footer to look less odd

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,8 +1,14 @@
 <template>
-  <div id="app">
+  <div id="app" class="site">
     <header>
-      <h1>🤓📚</h1>
+      <h1>GxIL</h1>
     </header>
+
+    <section class="menu" :class="{ reveal: showNav }">
+      <button class="trigger" @click="toggleNav">Menu</button>
+
+      <main-nav />
+    </section>
 
     <main>
       <template v-if="this.loading || this.hasErrors">
@@ -16,8 +22,14 @@
     </main>
 
     <footer>
-      <p>
-        &ldquo;{{ this.quote.body }}&rdquo; &mdash; {{ this.quote.author }}
+      <div class="rule">
+        <div class="before"></div>
+        <div class="text">fin.</div>
+        <div class="after"></div>
+      </div>
+      <p class="quote">
+        &ldquo;{{ this.quote.body }}&rdquo;
+        <span class="author">&mdash; {{ this.quote.author }}</span>
       </p>
     </footer>
   </div>
@@ -25,11 +37,12 @@
 
 <script>
   import Loader from './components/loader.vue';
+  import MainNav from './components/main-nav.vue';
   import Books from './components/books.vue';
   import axios from 'axios';
 
   export default {
-    components: { Books, Loader },
+    components: { Books, Loader, MainNav },
     data() {
       return {
         loading: false,
@@ -38,7 +51,8 @@
         quote: {
           body: 'I wish you way more than luck.',
           author: 'David Foster Wallace'
-        }
+        },
+        showNav: false
       }
     },
     created() {
@@ -60,22 +74,29 @@
           .then(({data}) => this.books = data)
           .catch(error => this.errors.push(error))
           .then(() => this.loading = false)
+      },
+      toggleNav() {
+        this.showNav = !this.showNav
       }
     }
   }
 </script>
 
 <style lang="scss" scoped>
-  #app {
+  .site {
     min-height: 100vh;
     display: flex;
     flex-direction: column;
+    background: hsl(0, 0%, 95%);
+    font-family: 'Merriweather', serif;
   }
 
   header {
-    background: hsl(0, 0%, 10%);
-    padding: 0.5em;
+    font-family: 'Pacifico', cursive;
+    padding: 1em;
     margin-bottom: 0.5em;
+    border-bottom: 1px solid hsl(0, 0%, 80%);
+    text-align: center;
 
     h1 {
       margin: 0;
@@ -87,14 +108,116 @@
     text-align: center;
   }
 
+  .menu {
+    display: none;
+  }
+
   footer {
-    background: hsl(0, 0%, 20%);
-    color: hsl(360, 100%, 100%);
+    color: hsl(0, 0%, 40%);
     padding: 1em;
     text-align: center;
-    font-size: 0.7em;
-    font-weight: 300;
     font-style: italic;
     margin-top: 0.5em;
+
+    .rule {
+      display: flex;
+      justify-content: center;
+      margin-bottom: 1.5em;
+
+      .text {
+        font-family: 'Pacifico', cursive;
+        font-size: 1.5em;
+        margin: 0 5px;
+      }
+
+      .before, .after {
+        position: relative;
+        top: 0.75em;
+        width: 15%;
+        min-width: 100px;
+        border-top: 1px solid hsl(0, 0%, 70%);
+      }
+    }
+
+    .quote {
+      font-size: 0.7em;
+      margin-top: 1em;
+
+      .author {
+        display: block;
+      }
+    }
+  }
+
+  @media only screen and (min-width: 650px) {
+    footer .quote .author {
+      display: inline;
+    }
+  }
+
+  @supports (grid-area: auto) {
+    @media only screen and (min-width: 60em) {
+      .site {
+        display: grid;
+        grid-template-areas:
+          "header header header"
+          "main main main"
+          "footer footer footer"
+      }
+
+      header {
+        grid-area: header;
+        margin-bottom: 0;
+      }
+
+      main {
+        grid-area: main;
+      }
+
+      .menu, .trigger {
+        background: hsl(0, 0%, 10%);
+        color: #fff;
+        font-family: 'Lato', serif;
+      }
+
+      .menu {
+        display: block;
+        position: relative;
+        grid-column: 1;
+        grid-row: 1 / 4;
+        width: 15em;
+        left: -13em;
+        font-size: 0.8em;
+        padding-top: 1em;
+
+        .trigger {
+          font-size: 0.6em;
+          transform: rotate(90deg);
+          border: none;
+          padding: 0.5em;
+          font-size: 1em;
+          position: fixed;
+          top: 1.5em;
+          left: 1em;
+        }
+
+        &.reveal {
+          left: 0;
+
+          .trigger {
+            left: 14em;
+          }
+
+          .main-nav {
+            position: fixed;
+          }
+        }
+      }
+
+      footer {
+        grid-area: footer;
+        margin-top: 0;
+      }
+    }
   }
 </style>
